24ce27a5d35ba8206542e28bccffe24e8d7ee928
marcinseweryn <marcin-seweryn@wp.pl>
2017-03-09 15:09:07 +0100

    Added a new graphic style to "ReportWindow".

--- BaseGUI/src/controllers/admin/ReportWindowController.java
+++ BaseGUI/src/controllers/admin/ReportWindowController.java
@@ -2,16 +2,34 @@ package controllers.admin;
 
 import java.io.IOException;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class ReportWindowController {
 
+    @FXML
+    private JFXDatePicker dateFrom, dateTo;
+
+    @FXML
+    private JFXButton submitButton, standardReportButton, mostPopularBooksButton, 
+    printToExcelButton, menuButton;
+
+    
+    
+    @FXML
+    void initialize(){
+
+    }
+    
     @FXML
     void menuAction(ActionEvent event) throws IOException {
     	Parent parent = FXMLLoader.load(getClass().getResource("/fxml/admin/MenuWindow.fxml"));
@@ -20,6 +38,78 @@ public class ReportWindowController {
     	
     	stage.setScene(scene);
     	stage.show();
+    	
+    }
+    
+    @FXML
+    void menuMouseEntered(MouseEvent event) {
+    	menuButton.setStyle("-fx-background-color:  #673ab7");
+    }
 
+    @FXML
+    void menuMouseExited(MouseEvent event) {
+    	menuButton.setStyle("-fx-background-color:  #2196f3");
+    }
+
+    @FXML
+    void mostPopularBooksAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostPopularBooksMouseEntered(MouseEvent event) {
+    	mostPopularBooksButton.setStyle("-fx-background-color:  #673ab7");
+    }
+
+    @FXML
+    void mostPopularBooksMouseExited(MouseEvent event) {
+    	mostPopularBooksButton.setStyle("-fx-background-color:  #2196f3");
+    }
+
+    @FXML
+    void printToExcelAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printToExcelMouseEntered(MouseEvent event) {
+    	printToExcelButton.setStyle("-fx-background-color:  #673ab7");
+    }
+
+    @FXML
+    void printToExcelMouseExited(MouseEvent event) {
+    	printToExcelButton.setStyle("-fx-background-color:  #2196f3");
+    }
+
+    @FXML
+    void standardReportAction(ActionEvent event) {
+    
+
+    }
+
+    @FXML
+    void standardReportMouseEntered(MouseEvent event) {
+    	standardReportButton.setStyle("-fx-background-color:  #673ab7");
+    }
+
+    @FXML
+    void standardReportMouseExited(MouseEvent event) {
+    	standardReportButton.setStyle("-fx-background-color:  #2196f3");
+    }
+
+    @FXML
+    void submitAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitMouseEntered(MouseEvent event) {
+    	submitButton.setStyle("-fx-background-color:  #673ab7");
+
+    }
+
+    @FXML
+    void submitMouseExited(MouseEvent event) {
+    	submitButton.setStyle("-fx-background-color:  #2196f3");
     }
 }
